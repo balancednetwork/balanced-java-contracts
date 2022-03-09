8da7b38b613137abbc93321476eec7f9f7b1c350
suyogadhikari <adhikarisuyog49@gmail.com>
2022-03-09 17:45:41 +0545

    unit test added

---
+++ core-contracts/staking/src/test/java/network/balanced/score/core/MockSicx.java
@@ -0,0 +1,23 @@
+package network.balanced.score.core;
+
+import score.Address;
+import score.Context;
+import score.VarDB;
+import score.annotation.External;
+
+import java.math.BigInteger;
+
+
+
+
+public class MockSicx {
+
+    public MockSicx() {
+    }
+
+    @External
+    public void tokenFallback(Address _from, BigInteger _value, byte[] _data) {
+
+    }
+
+}
