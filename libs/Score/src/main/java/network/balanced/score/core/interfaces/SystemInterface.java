dc60ddb12c50491a1ec279d496f75270b825874e
suyogadhikari <adhikarisuyog49@gmail.com>
2022-03-10 08:52:13 +0545

    new interface added

--- libs/Score/src/main/java/network/balanced/score/core/interfaces/SystemInterface.java
+++ libs/Score/src/main/java/network/balanced/score/core/interfaces/SystemInterface.java
@@ -8,6 +8,8 @@ import java.util.Map;
 public interface SystemInterface {
     Map<String, Object> getIISSInfo();
 
+    Map<String, Object> queryIScore(Address address);
+
     Map<String, Object> getStake(Address address);
 
     Map<String, Object> getDelegation(Address address);
