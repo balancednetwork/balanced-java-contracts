03853a18d2596c98d3b59120ffbfee7068315c92
Night Owl <nightowl121@protonmail.com>
2022-03-10 13:47:44 +0545

    Optimize import in ScoreIntegrationTest
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- libs/TestUtils/src/main/java/network/balanced/score/core/test/ScoreIntegrationTest.java
+++ libs/TestUtils/src/main/java/network/balanced/score/core/test/ScoreIntegrationTest.java
@@ -1,7 +1,20 @@
-package network.balanced.score.core.test;
+/*
+ * Copyright (c) 2022 Balanced.network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import static foundation.icon.jsonrpc.IconJsonModule.hexToBytes;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+package network.balanced.score.core.test;
 
 import foundation.icon.icx.KeyWallet;
 import foundation.icon.icx.Wallet;
@@ -9,6 +22,12 @@ import foundation.icon.icx.data.TransactionResult;
 import foundation.icon.jsonrpc.Address;
 import foundation.icon.score.client.DefaultScoreClient;
 import foundation.icon.score.client.RevertedException;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.function.Executable;
+import score.UserRevertedException;
+
 import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.NoSuchAlgorithmException;
@@ -16,17 +35,13 @@ import java.security.NoSuchProviderException;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Random;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.function.Executable;
-import score.UserRevertedException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Tag("integration")
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
