07d7731a0a44a316fc34a5e9f032dacaacc3178b
Sang Lai <sanglt.cdn@gmail.com>
2022-03-19 23:29:51 +0700

    feat: translate multical score from solidity to java

--- core-contracts/Multicall/src/main/java/network/balanced/score/core/multicall/Multicall.java
+++ core-contracts/Multicall/src/main/java/network/balanced/score/core/multicall/Multicall.java
@@ -35,6 +35,22 @@ public class Multicall {
     public Multicall() {
     }
 
+    public static class Call {
+        public Address target;
+        public String method;
+        public String[] params;
+    }
+
+    public static class Result {
+        public Result(boolean b, Object result) {
+            this.success = b;
+            this.returnData = result;
+        }
+
+        public boolean success;
+        public Object returnData;
+    }
+
     private void onlyOwner() {
         Address caller = Context.getCaller();
         Address owner = Context.getOwner();
@@ -72,6 +88,119 @@ public class Multicall {
         poolStatsWithId.putAll(poolStats);
 
         return poolStatsWithId;
+    }
+
+    private static Object[] convertToType(String[] params) {
+        Object[] results = new Object[params.length];
+        for (int i = 0; i < params.length; i++) {
+            if (params[i].startsWith("hx") || params[i].startsWith("cx")) {
+                results[i] = Address.fromString(params[i]);
+            } else if (params[i].equals("false") || params[i].equals("true")) {
+                results[i] = Boolean.parseBoolean(params[i]);
+            } else if (params[i].startsWith("0x")) {
+                results[i] = Long.decode(params[i]).longValue();
+            } else {
+                results[i] = params[i];
+            }
+        }
+        return results;
+    }
+
+    @External(readonly = true)
+    public Map<String, Object> aggregate(Call[] calls) {
+        long blocklNumber = Context.getBlockHeight();
+        Object[] returnData = new Object[calls.length];
+
+        for (int i = 0; i < calls.length; i++) {
+            Object[] params = convertToType(calls[i].params);
+            Address contractAddress = calls[i].target;
+            String method = calls[i].method;
+
+            try {
+                if (calls[i].params.length == 0) {
+                    returnData[i] = Context.call(contractAddress, method);
+                } else {
+                    returnData[i] = Context.call(contractAddress, method, params);
+                }
+            } catch (Exception e) {
+                Context.println(e.getMessage());
+                Context.revert(e + ": Multicall aggregate: call failed " + contractAddress + " method: " + method);
+            }
+        }
+        return Map.of("blockNumber", blocklNumber, "returnData", returnData);
+
+    }
+
+    @External(readonly = true)
+    public Map<String, Object> tryAggregate(boolean requireSuccess, Call[] calls) {
+        Result[] returnData = new Result[calls.length];
+        for (int i = 0; i < calls.length; i++) {
+            Object[] params = convertToType(calls[i].params);
+            Address contractAddress = calls[i].target;
+            String method = calls[i].method;
+
+            try {
+                Object result = new Object();
+                if (calls[i].params.length == 0) {
+                    result = Context.call(contractAddress, method);
+                } else {
+                    result = Context.call(contractAddress, method, params);
+                }
+                returnData[i] = new Result(Boolean.TRUE, result);
+            } catch (Exception e) {
+                String errMessage = "Multicall tryAggregate: call failed " + contractAddress + " method: " + method;
+                if (requireSuccess) {
+                    Context.println(e.getMessage());
+                    Context.revert(
+                            e + ": " + errMessage);
+                }
+                returnData[i] = new Result(Boolean.FALSE, errMessage);
+            }
+        }
+        return Map.of("returnData", returnData);
+    }
+
+    @External(readonly = true)
+    public Map<String, Object> tryBlockAndAggregate(boolean requireSuccess,
+            Call[] calls) {
+        long blocklNumber = getBlockNumber();
+        byte[] blockHash = getLastBlockHash();
+        Map<String, Object> returnData = tryAggregate(requireSuccess, calls);
+
+        return Map.of(
+                "blockNumber", blocklNumber,
+                "blockHash", blockHash,
+                "returnData", returnData.get("returnData"));
+    }
+
+    @External(readonly = true)
+    public Map<String, Object> blockAndAggregate(Call[] calls) {
+        return tryBlockAndAggregate(Boolean.TRUE, calls);
+    }
+
+    @External(readonly = true)
+    public BigInteger getBalance(Address addr) {
+        return Context.getBalance(addr);
+    }
+
+    @External(readonly = true)
+    public byte[] getBlockHash(long blockNumber) {
+        return Context.hash("sha3-256", BigInteger.valueOf(blockNumber).toByteArray());
+    }
+
+    @External(readonly = true)
+    public byte[] getLastBlockHash() {
+        return Context.hash("sha3-256", BigInteger.valueOf(Context.getBlockHeight() - 1).toByteArray());
+    }
+
+    @External(readonly = true)
+    public long getCurrentBlockTimestamp() {
+        return Context.getBlockTimestamp();
+    }
+
+    @External(readonly = true)
+    public long getBlockNumber() {
+        return Context.getBlockHeight();
     }
 
 }
