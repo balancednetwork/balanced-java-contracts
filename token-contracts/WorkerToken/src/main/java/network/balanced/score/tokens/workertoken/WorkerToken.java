17b39e8c5489e88464710581949f4455943e9eef
Night Owl <nightowl121@protonmail.com>
2022-03-30 17:16:31 +0545

    Refactor package name and project name of Worker Token
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- token-contracts/WorkerToken/src/main/java/network/balanced/score/tokens/workertoken/WorkerToken.java
+++ token-contracts/WorkerToken/src/main/java/network/balanced/score/tokens/workertoken/WorkerToken.java
@@ -1,17 +1,33 @@
-package network.balanced.score.tokens;
+/*
+ * Copyright (c) 2022-2022 Balanced.network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.math.BigInteger;
+package network.balanced.score.tokens.workertoken;
 
-
-import network.balanced.score.tokens.tokens.Checks;
-import network.balanced.score.tokens.tokens.IRC2;
+import network.balanced.score.tokens.workertoken.utils.Checks;
+import network.balanced.score.tokens.workertoken.utils.IRC2;
 import score.Address;
 import score.Context;
 import score.VarDB;
 import score.annotation.External;
 import score.annotation.Optional;
 
-import static network.balanced.score.tokens.tokens.Checks.*;
+import java.math.BigInteger;
+
+import static network.balanced.score.tokens.workertoken.utils.Checks.onlyAdmin;
+import static network.balanced.score.tokens.workertoken.utils.Checks.onlyGovernance;
 
 public class WorkerToken extends IRC2 {
     private static String TOKEN_NAME = "Balanced Worker Token";
