03b0da69399594116c978af86856019e1d28ab89
Night Owl <nightowl121@protonmail.com>
2022-04-07 13:27:52 +0545

    Use score lib for authorization checks
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- core-contracts/Router/src/main/java/network/balanced/score/core/router/Router.java
+++ core-contracts/Router/src/main/java/network/balanced/score/core/router/Router.java
@@ -1,5 +1,23 @@
+/*
+ * Copyright (c) 2022 Balanced.network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package network.balanced.score.core.router;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonValue;
 import score.Address;
 import score.Context;
@@ -7,20 +25,22 @@ import score.VarDB;
 import score.annotation.External;
 import score.annotation.Optional;
 import score.annotation.Payable;
-import com.eclipsesource.json.Json;
-import com.eclipsesource.json.JsonObject;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.List;
+
+import static network.balanced.score.lib.utils.Check.*;
 
 public class Router {
-    private static String DEX_ADDRESS = "dex_address";
-    private static String SICX_ADDRESS = "sicx_address";
-    private static String STAKING_ADDRESS = "staking_address";
-    private static String GOVERNANCE_ADDRESS = "governance_address";
-    private static String ADMIN = "admin";
-    private static BigInteger MAX_NUMBER_OF_ITERATIONS = BigInteger.valueOf(4L); // Unbounded iterations won"t pass audit
-    private static Address MINT_ADDRESS = Address.fromString("hx0000000000000000000000000000000000000000");
+    private static final String DEX_ADDRESS = "dex_address";
+    private static final String SICX_ADDRESS = "sicx_address";
+    private static final String STAKING_ADDRESS = "staking_address";
+    private static final String GOVERNANCE_ADDRESS = "governance_address";
+    private static final String ADMIN = "admin";
+
+    private static final int MAX_NUMBER_OF_ITERATIONS = 4;
+    private static final Address MINT_ADDRESS = new Address(new byte[Address.LENGTH]);
     private final String TAG = "Balanced Router";
 
     VarDB<Address> governance = Context.newVarDB(GOVERNANCE_ADDRESS, Address.class);
@@ -29,13 +49,10 @@ public class Router {
     VarDB<Address> staking = Context.newVarDB(STAKING_ADDRESS, Address.class);
     VarDB<Address> dex = Context.newVarDB(DEX_ADDRESS, Address.class);
 
-    public Router(@Optional Address _governance) {
-        if(_governance != null) {
+    public Router(Address _governance) {
+        if (governance.get() == null) {
+            isContract(_governance);
             governance.set(_governance);
-            admin.set(_governance);
-            dex.set(Address.fromString("cxa0af3165c08318e988cb30993b3048335b94af6c"));
-            sicx.set(Address.fromString("cx2609b924e33ef00b648a409245c7ea394c467824"));
-            staking.set(Address.fromString("cx43e2eec79eb76293c298f2b17aec06097be606e0"));
         }
     }
 
@@ -44,50 +61,26 @@ public class Router {
         return TAG;
     }
 
-    /**
-     *  Gets the current admin address. This user can call using the
-     *         `@only_admin` decorator.
-     * @return Returns the address of the current admin
-     */
     @External(readonly = true)
     public Address getAdmin() {
         return admin.get();
     }
 
-    /**
-     *
-     * @param _admin  The new admin address to set.
-     *         Can make calls with the `@only_admin` decorator.
-     *         Should be called before DEX use.
-     */
     @External
     public void setAdmin(Address _admin) {
-        Context.require(
-                Context.getCaller() == governance.get(),
-                "Only governance score can call this method"
-        );
+        only(governance);
         admin.set(_admin);
     }
 
-    /**
-     *
-     * @return Address of the dex score
-     */
     @External(readonly = true)
     public Address getDex() {
         return dex.get();
     }
 
-    /**
-     *
-     * @param _dex The new DEX address to set.
-     */
     @External
     public void setDex(Address _dex) {
-        Context.require(
-                Context.getCaller() == governance.get(),
-                "Only governance score can call this method"
-        );
+        only(admin);
+        isContract(_dex);
         dex.set(_dex);
     }
 
@@ -98,27 +91,15 @@ public class Router {
 
     @External
     public void setSicx(Address _address) {
-        Context.require(
-                Context.getCaller() == admin.get(),
-                "Only governance address can call this method"
-        );
-        Context.require(_address.isContract(), TAG +
-                "Address provided is an EOA address. A contract address is required.");
+        only(admin);
+        isContract(_address);
         sicx.set(_address);
     }
 
-    /**
-     * Sets new Governance contract address. Should be called before dex use.
-     * @param _address New contract address to set.
-     */
     @External
     public void setGovernance(Address _address) {
-        Context.require(
-                Context.getCaller() == Context.getOwner(),
-                "Only g governance call this method"
-        );
-        Context.require(_address.isContract(), TAG +
-                "Address provided is an EOA address. A contract address is required.");
+        onlyOwner();
+        isContract(_address);
         governance.set(_address);
     }
 
@@ -127,37 +108,22 @@ public class Router {
         return governance.get();
     }
 
-    /**
-     *
-     * @return Gets the address of the Staking contract.
-     */
     @External(readonly = true)
     public Address getStaking() {
         return staking.get();
     }
 
-    /**
-     * Sets new Governance contract address. Should be called before dex use.
-     * @param _address New contract address to set.
-     */
     @External
     public void setStaking(Address _address) {
-        Context.require(
-                Context.getCaller() == admin.get(),
-                "Only governance address can call this method"
-        );
-        Context.require(_address.isContract(), TAG +
-                "Address provided is an EOA address. A contract address is required.");
+        only(admin);
+        isContract(_address);
         staking.set(_address);
     }
 
 
     void swap(Address _fromToken, Address _toToken) {
         if(_fromToken == null) {
-            Context.require(
-                    _toToken == sicx.get(),
-                    TAG + ": ICX can only be traded for sICX"
-            );
+            Context.require(_toToken == sicx.get(), TAG + ": ICX can only be traded for sICX");
             BigInteger balance = Context.getBalance(Context.getAddress());
             Context.transfer(staking.get(), balance);
         }
@@ -168,10 +134,7 @@ public class Router {
         }
         else {
             BigInteger balance = (BigInteger) Context.call(_fromToken, "balanceOf");
-            String data = "" +
-                    "{\"method\":\"_swap\",\"params\":{\"toToken\":\""
-                    + _toToken
-                    + "\"}}";
+            String data = "{\"method\":\"_swap\",\"params\":{\"toToken\":\"" + _toToken + "\"}}";
             Context.call(_fromToken, "transfer", balance, data.getBytes());
         }
     }
@@ -186,27 +149,20 @@ public class Router {
 
         if(currentToken == null) {
             BigInteger balance = Context.getBalance(Context.getAddress());
-            Context.require(
-                    balance.compareTo(_minReceive) >= 0,
-                    TAG + ":Below minimum receive amount of" + _minReceive
-            );
+            Context.require(balance.compareTo(_minReceive) >= 0,
+                    TAG + ": Below minimum receive amount of" + _minReceive);
             Context.transfer(_from, balance);
         }
         else {
             BigInteger balance = (BigInteger) Context.call(currentToken, "balanceOf", Context.getAddress());
-            Context.require(
-                    balance.compareTo(_minReceive) >= 0,
-                    TAG + ":Below minimum receive amount of" + _minReceive
-            );
-            String data = "";
-            Context.call(currentToken, "transfer", _from, balance, data.getBytes());
+            Context.require(balance.compareTo(_minReceive) >= 0,
+                    TAG + ":Below minimum receive amount of" + _minReceive);
+            Context.call(currentToken, "transfer", _from, balance, new byte[0]);
         }
     }
 
     /**
-     * Uses DEX for exchange in the path where highest return can be obtained
-     * @param _path
-     * @param _minReceive
+     * Uses DEX for exchange in the path where the highest return can be obtained
      */
     @Payable
     @External
@@ -215,17 +171,15 @@ public class Router {
             _minReceive = BigInteger.ZERO;
         }
 
-        Context.require(
-                _path.length <= MAX_NUMBER_OF_ITERATIONS.intValue(),
-                "Passed max swaps of" + MAX_NUMBER_OF_ITERATIONS
-        );
+        Context.require(_path.length <= MAX_NUMBER_OF_ITERATIONS,
+                TAG + ": Passed max swaps of" + MAX_NUMBER_OF_ITERATIONS);
 
         route(Context.getCaller(), null, _path, _minReceive);
     }
 
     /**
      *  This is invoked when a token is transferred to this score.
-     *      It expects a JSON object with the following format:
+     *  It expects a JSON object with the following format:
      *      ```
      *      {"method": "METHOD_NAME", "params":{...}}
      *      ```
@@ -254,34 +208,26 @@ public class Router {
         String method = json.get("method").asString();
         JsonObject params = json.get("params").asObject();
 
-        Context.require(
-                method.contains("_swap"),
-                TAG + "Fallback directly not allowed."
-        );
+        Context.require(method.contains("_swap"), TAG + ": Fallback directly not allowed.");
         BigInteger minimumReceive = BigInteger.ZERO;
         if (params.contains("minimumReceive")) {
             minimumReceive = BigInteger.valueOf(params.get("minimumReceive").asInt());
-            Context.require(
-                    minimumReceive.signum() >= 0,
-                    TAG + "Must specify a positive number for minimum to receive"
-            );
+            Context.require(minimumReceive.signum() >= 0, TAG + ": Must specify a positive number for minimum to " +
+                    "receive");
         }
         Address receiver;
-        if(params.contains("receiver")) {
+        if (params.contains("receiver")) {
             receiver = Address.fromString(params.get("receiver").asString());
-        }
-        else  {
+        } else {
             receiver = _from;
         }
 
-        Context.require(
-                params.get("path").asArray().size() > MAX_NUMBER_OF_ITERATIONS.intValue(),
-                TAG +"Passed max swaps of " + MAX_NUMBER_OF_ITERATIONS
-                );
+        Context.require(params.get("path").asArray().size() <= MAX_NUMBER_OF_ITERATIONS,
+                TAG + ": Passed max swaps of " + MAX_NUMBER_OF_ITERATIONS);
 
-        ArrayList<Address> path = new ArrayList<>();
+        List<Address> path = new ArrayList<>();
 
-        for(JsonValue addressJsonValue: params.get("path").asArray()) {
+        for (JsonValue addressJsonValue : params.get("path").asArray()) {
             if (addressJsonValue != null) {
                 path.add(Address.fromString(addressJsonValue.asString()));
             }
--- core-contracts/Router/src/test/java/network/balanced/score/core/router/RouterTest.java
+++ core-contracts/Router/src/test/java/network/balanced/score/core/router/RouterTest.java
@@ -1,26 +1,40 @@
+/*
+ * Copyright (c) 2022 Balanced.network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package network.balanced.score.core.router;
 
 import com.iconloop.score.test.Account;
 import com.iconloop.score.test.Score;
 import com.iconloop.score.test.ServiceManager;
 import com.iconloop.score.test.TestBase;
-import com.iconloop.score.token.irc2.IRC2Basic;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import score.Address;
-import score.annotation.External;
 
 import java.math.BigInteger;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class RouterTest extends TestBase {
     private static final ServiceManager sm = getServiceManager();
-    private static Account owner = sm.createAccount();
+    private static final Account owner = sm.createAccount();
     private static Score routerScore;
-    private static Account adminAccount = sm.createAccount();
-    private static Account governanceScore = Account.newScoreAccount(0);
+    private static final Account adminAccount = sm.createAccount();
+    private static final Account governanceScore = Account.newScoreAccount(0);
 
     @BeforeAll
     static void setup() throws Exception {
@@ -91,7 +105,7 @@ public class RouterTest extends TestBase {
     @Test
     void testRoute() throws Exception {
         Account account1 = Account.newScoreAccount(3);
-        // cannnot transfer balance into score create by using Account.newScoreAccount()
+        // cannot transfer balance into score create by using Account.newScoreAccount()
         // so we need to use a score created by us
         // hence we use routerScore
 //        Account token = Account.newScoreAccount(4);
