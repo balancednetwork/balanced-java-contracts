f89d3aec840f78578b20aed0e54da15af340de5c
Night Owl <nightowl121@protonmail.com>
2022-04-21 18:57:59 +0545

    Use refactored IRC2 mintable and burnable classes
    * Add tests for balanced dollar
    * Add test helper method for general admin control methods
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- test-lib/src/main/java/network/balanced/score/lib/test/DummyOracle.java
+++ test-lib/src/main/java/network/balanced/score/lib/test/DummyOracle.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package network.balanced.score.tokens;
+package network.balanced.score.lib.test;
 
 import score.annotation.External;
 import scorex.util.HashMap;
