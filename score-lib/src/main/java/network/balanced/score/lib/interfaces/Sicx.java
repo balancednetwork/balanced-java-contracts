3449cadcc453a0296b058b2373a253c02275cf1f
suyogadhikari <adhikarisuyog49@gmail.com>
2022-04-25 08:03:01 +0545

    sicx Interface modified

--- score-lib/src/main/java/network/balanced/score/lib/interfaces/Sicx.java
+++ score-lib/src/main/java/network/balanced/score/lib/interfaces/Sicx.java
@@ -1,12 +1,11 @@
 package network.balanced.score.lib.interfaces;
-
-import network.balanced.score.lib.interfaces.addresses.AdminAddress;
 import network.balanced.score.lib.interfaces.addresses.StakingAddress;
+import network.balanced.score.lib.interfaces.base.IRC2BurnableInterface;
 import score.annotation.External;
 
 import java.math.BigInteger;
 
-public interface Sicx extends StakingAddress{
+public interface Sicx extends StakingAddress, IRC2BurnableInterface {
 
     @External(readonly = true)
     String getPeg();
