3aee460278733f68025c5c708c51d88d5582d19e
Night Owl <nightowl121@protonmail.com>
2022-05-13 13:25:51 +0545

    Apply review comments
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- util-contracts/Stability/src/main/java/network/balanced/score/util/stability/Stability.java
+++ util-contracts/Stability/src/main/java/network/balanced/score/util/stability/Stability.java
@@ -16,10 +16,7 @@
 
 package network.balanced.score.util.stability;
 
-import score.Address;
-import score.Context;
-import score.DictDB;
-import score.VarDB;
+import score.*;
 import score.annotation.External;
 
 import java.math.BigInteger;
@@ -34,7 +31,7 @@ public class Stability {
     private static final String TAG = "Balanced Peg Stability";
     private static final Address EOA_ZERO_ADDRESS = new Address(new byte[Address.LENGTH]);
     private static final BigInteger HUNDRED_PERCENTAGE = BigInteger.valueOf(100).multiply(EXA);
-    private static final int bnusdDecimals = 18;
+    private static final BigInteger ONE_BNUSD = EXA;
 
     private static final String FEE_HANDLER_ADDRESS = "fee_handler_address";
     private static final String TOKEN_LIMIT = "token_limit";
@@ -88,7 +85,7 @@ public class Stability {
     @External
     public void setFeeIn(BigInteger _feeIn) {
         onlyOwner();
-        Context.require(_feeIn.compareTo(BigInteger.ZERO) >= 0, TAG + ": Fee in can't be negative");
+        isValidPercentage(_feeIn);
         feeIn.set(_feeIn);
     }
 
@@ -100,8 +97,14 @@ public class Stability {
     @External
     public void setFeeOut(BigInteger _feeOut) {
         onlyOwner();
-        Context.require(_feeOut.compareTo(BigInteger.ZERO) >= 0, TAG + ": Fee out can't be negative");
+        isValidPercentage(_feeOut);
         feeOut.set(_feeOut);
+    }
+
+    private void isValidPercentage(BigInteger _value) {
+        Context.require(_value.compareTo(BigInteger.ZERO) >= 0, TAG + ": Percentage can't be negative");
+        Context.require(_value.compareTo(HUNDRED_PERCENTAGE) <= 0, TAG + ": Percentage can't be greater than hundred " +
+                "percentage.");
     }
 
     @External(readonly = true)
@@ -110,13 +113,14 @@ public class Stability {
     }
 
     @External
-    public void whitelistTokens(Address address, BigInteger limit) {
+    public void whitelistTokens(Address _address, BigInteger _limit) {
         onlyOwner();
-        isContract(address);
-        Context.require(limit.compareTo(BigInteger.ZERO) >= 0, TAG + ": Limit can't be set negative");
-        tokenLimits.set(address, limit);
-        int tokenDecimal = (int) Context.call(address, "decimals");
-        decimals.set(address, tokenDecimal);
+        isContract(_address);
+        Context.require(_limit.compareTo(BigInteger.ZERO) >= 0, TAG + ": Limit can't be set negative");
+        int tokenDecimal = (int) Context.call(_address, "decimals");
+        decimals.set(_address, tokenDecimal);
+        BigInteger actualLimit = _limit.multiply(pow(BigInteger.TEN, tokenDecimal));
+        tokenLimits.set(_address, actualLimit);
     }
 
     @External
@@ -134,28 +138,33 @@ public class Stability {
 
     private void mintBnusd(BigInteger _amount, Address _asset, Address _user, Address bnusdAddress) {
         int assetInDecimals = decimals.get(_asset);
-        BigInteger equivalentBnusd = (_amount.multiply(pow(BigInteger.TEN, bnusdDecimals))).divide(pow(BigInteger.TEN
-                , assetInDecimals));
+        BigInteger equivalentBnusd = (_amount.multiply(ONE_BNUSD)).divide(pow(BigInteger.TEN, assetInDecimals));
+        Context.require(equivalentBnusd.compareTo(BigInteger.ZERO) > 0, TAG + ": Bnusd amount must be greater than " +
+                "zero");
         BigInteger fee = (feeIn.get().multiply(equivalentBnusd)).divide(HUNDRED_PERCENTAGE);
 
         Context.call(bnusdAddress, "mint", equivalentBnusd);
         Context.call(bnusdAddress, "transfer", feeHandler.get(), fee);
         Context.call(bnusdAddress, "transfer", _user, equivalentBnusd.subtract(fee));
-
     }
 
     private void returnBnusd(Address _user, BigInteger _amount, byte[] _data, Address bnusdAddress) {
         String asset = new String(_data);
         Address assetToReturn = Address.fromString(asset);
 
+        Context.require(tokenLimits.get(assetToReturn) != null, TAG + ": Whitelisted tokens can only be sent");
+
         int assetOutDecimals = decimals.get(assetToReturn);
         BigInteger fee = (feeOut.get().multiply(_amount)).divide(HUNDRED_PERCENTAGE);
         BigInteger bnusdToConvert = _amount.subtract(fee);
         BigInteger equivalentAssetAmount =
-                (bnusdToConvert.multiply(pow(BigInteger.TEN, assetOutDecimals))).divide(pow(BigInteger.TEN,
-                        bnusdDecimals));
+                (bnusdToConvert.multiply(pow(BigInteger.TEN, assetOutDecimals))).divide(ONE_BNUSD);
         Context.require(equivalentAssetAmount.compareTo(BigInteger.ZERO) > 0, TAG + ": Asset to return can't be zero " +
                 "or less");
+
+        BigInteger assetOutBalance = (BigInteger) Context.call(assetToReturn, "balanceOf", Context.getAddress());
+        Context.require(equivalentAssetAmount.compareTo(assetOutBalance) <= 0, TAG + ": Insufficient asset out " +
+                "balance in the contract");
 
         Context.call(bnusdAddress, "burn", bnusdToConvert);
         Context.call(bnusdAddress, "transfer", feeHandler.get(), fee);
@@ -164,26 +173,25 @@ public class Stability {
 
     @External
     public void tokenFallback(Address _from, BigInteger _value, byte[] _data) {
+        if (_from.equals(EOA_ZERO_ADDRESS)) {
+            return;
+        }
+
         Address token = Context.getCaller();
         BigInteger limit = tokenLimits.get(token);
         Address bnusdAddress = this.bnusdAddress.get();
         Context.require(_value.compareTo(BigInteger.ZERO) > 0, TAG + ": Transfer amount must be greater than zero");
-        Context.require(limit != null || token.equals(bnusdAddress), TAG + ": Only whitelisted tokens or bnusd is " +
-                "accepted.");
 
-        if (limit != null) {
+        if (token.equals(bnusdAddress)) {
+            returnBnusd(_from, _value, _data, token);
+        } else if (limit != null) {
             BigInteger assetInBalance = (BigInteger) Context.call(token, "balanceOf", Context.getAddress());
             Context.require(assetInBalance.add(_value).compareTo(limit) <= 0, TAG + ": Asset to exchange with bnusd " +
                     "limit crossed.");
+            mintBnusd(_value, token, _from, bnusdAddress);
+        } else {
+            throw new UserRevertException(TAG + ": Only whitelisted tokens or bnusd is accepted.");
         }
 
-        if (token.equals(bnusdAddress)) {
-            if (_from.equals(EOA_ZERO_ADDRESS)) {
-                return;
-            }
-            returnBnusd(_from, _value, _data, token);
-        } else {
-            mintBnusd(_value, token, _from, bnusdAddress);
-        }
     }
 }
