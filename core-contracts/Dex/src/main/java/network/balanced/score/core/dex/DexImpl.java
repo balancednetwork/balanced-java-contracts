99cf25389d71ed8c1ccb2c24ee9b4a6217a3d7a8
Night Owl <nightowl121@protonmail.com>
2022-05-25 15:09:40 +0545

    Avoid fetching staked balance twice for a user
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- core-contracts/Dex/src/main/java/network/balanced/score/core/dex/DexImpl.java
+++ core-contracts/Dex/src/main/java/network/balanced/score/core/dex/DexImpl.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2022 Balanced.network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package network.balanced.score.core.dex;
 
 
@@ -1258,8 +1274,6 @@ public class DexImpl {
         Map<String, Object> snapshotData = new HashMap<>();
         for (Address addr : activeAddresses.get(_id).range(_offset, _offset.add(_limit))) {
             BigInteger snapshotBalance = balanceOf(addr, _id);
-            BigInteger stakedBalance = (BigInteger) Context.call(stakedlp.get(), "balanceOf", addr, _id);
-            snapshotBalance = snapshotBalance.add(stakedBalance);
             if (!snapshotBalance.equals(BigInteger.ZERO)) {
                 snapshotData.put(addr.toString(), snapshotBalance);
             }
