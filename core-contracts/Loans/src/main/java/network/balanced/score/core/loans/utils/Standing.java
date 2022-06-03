7da7d747c42a33498069331f6fa4b4e5fcebdfa5
AntonAndell <anton@andell.eu>
2022-06-03 09:01:48 +0200

    remove usage of Map.of

--- core-contracts/Loans/src/main/java/network/balanced/score/core/loans/utils/Standing.java
+++ core-contracts/Loans/src/main/java/network/balanced/score/core/loans/utils/Standing.java
@@ -2,6 +2,7 @@ package network.balanced.score.core.loans.utils;
 
 import java.math.BigInteger;
 import java.util.Map;
+import scorex.util.HashMap;
 
 import static network.balanced.score.core.loans.utils.LoansConstants.StandingsMap;
 import static network.balanced.score.core.loans.utils.LoansConstants.Standings;
@@ -13,11 +14,12 @@ public class Standing {
     public Standings standing;
 
     public Map<String, Object> toMap() {
-        return Map.of(
-            "collateral", collateral,
-            "debt", totalDebt,
-            "ratio", ratio,
-            "standing", StandingsMap.get(standing)
-        );
+        Map<String, Object> standingData = new HashMap<>();
+        standingData.put("collateral", collateral);
+        standingData.put("debt", totalDebt);
+        standingData.put("ratio", ratio);
+        standingData.put("standing", StandingsMap.get(standing));
+
+        return standingData;
     }
 }
