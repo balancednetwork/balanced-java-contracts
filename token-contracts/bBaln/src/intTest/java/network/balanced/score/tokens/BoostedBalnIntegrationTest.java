58ccb8c0d3f08573c8df52fbf1f6e92c9012a37f
sagarsapkota <sapkotasagar51@yahoo.com>
2022-06-10 12:07:35 +0545

    minor update in build.gradle

---
+++ token-contracts/bBaln/src/intTest/java/network/balanced/score/tokens/BoostedBalnIntegrationTest.java
@@ -0,0 +1,11 @@
+package network.balanced.score.tokens;
+
+
+import network.balanced.score.lib.test.integration.ScoreIntegrationTest;
+
+public class BoostedBalnIntegrationTest implements ScoreIntegrationTest {
+
+
+
+
+}
