925c23e50c972749b84ce02abb53cb984e5952cb
ZeChosenName <zechosen_username@protonmail.com>
2022-07-02 14:32:59 +0545

    Mainnet address added for deployment

---
+++ buildSrc/src/main/java/network/balanced/score/dependencies/Addresses.java
@@ -0,0 +1,50 @@
+package network.balanced.score.dependencies;
+
+public class Addresses {
+
+      public class sejong {
+            public static final String oracle = "cx1c89400fb97b74f9f6d6dc04d3135076f8658abe";
+            public static final String multicall = "cx89ea649431a9db19115d292778d93d2d0ebad476";
+            public static final String governance = "cx7bbb16f746aae9d9f4fdc6d39a2a5144b123b18a";
+            public static final String baln = "cxf4d8a45fedf5efc7420b7e4a4a0a8dff94269476";
+            public static final String bnUSD = "cx33b73f45e494302c5098c32c36491ee39e5c6af4";
+            public static final String worker_token = "cxe13633d39c116d7fe21a71eb68f16b541300afa4";
+            public static final String daofund = "cx4cd1c3e3eb3d4cc6b821d650749c626c04f4834c";
+            public static final String dex = "cx7cbf2fdb26cd7d0175a3253fbbd2a0b26c6bbd9f";
+            public static final String disbursement = "cx800b0b1a28887ef5d787c06afbaf5b3826c2de13";
+            public static final String dividends = "cxb328686d29e8c294a3a29088ab8b0131352bbe1f";
+            public static final String feehandler = "cxcf7e5853ad79a5c22ce2cf4f5b2acf25c299d68e";
+            public static final String loans = "cx2f9d0286ff8f26244e0c4f6766ce09bacf5d772e";
+            public static final String rebalancing = "cx6323168d22fcee51232f9924607cbbd69f767602";
+            public static final String reserve = "cxeb7c7bc04653bb51fab18e5bffca74ca5534889c";
+            public static final String rewards = "cx5b7f7355035114543d457fae5e0d80f2cee51029";
+            public static final String router = "cxdb9f1e186b7144fc9d3292cbb6f6d07dbfc85897";
+            public static final String stakedLp = "cxa18b5861b7b7d9a30de32b6950d74cb2a61a74fe";
+            public static final String stability = "cx38dd16a7184cac760c5bb536afb34a0424148329";
+            public static final String staking = "cxe1fb71c7b15a0c70d7ec4b49fd8809c6e75e63c9";
+            public static final String sicx = "cxfaa42fe413dc2248c3e27ad7598903a844cddfdf";
+      }
+
+      public class mainnet {
+            public static final String governance = "cx44250a12074799e26fdeee75648ae47e2cc84219";
+            public static final String baln = "cxf61cd5a45dc9f91c15aa65831a30a90d59a09619";
+            public static final String bnUSD = "cx88fd7df7ddff82f7cc735c871dc519838cb235bb";
+            public static final String bwt = "cxcfe9d1f83fa871e903008471cca786662437e58d";
+            public static final String daofund = "cx835b300dcfe01f0bdb794e134a0c5628384f4367";
+            public static final String dex = "cxa0af3165c08318e988cb30993b3048335b94af6c";
+            public static final String disbursement = "cxe3905591929d17fc8496ae28ee3b9c144579228e";
+            public static final String dividends = "cx203d9cd2a669be67177e997b8948ce2c35caffae";
+            public static final String feehandler = "cx5faae53c4dbd1fbe4a2eb4aab6565030f10da5c6";
+            public static final String stability = "cxa09dbb60dcb62fffbd232b6eae132d730a2aafa6";
+            public static final String loans = "cx66d4d90f5f113eba575bf793570135f9b10cece1";
+            public static final String oracle = "cxe647e0af68a4661566f5e9861ad4ac854de808a2";
+            public static final String rebalancing = "cx40d59439571299bca40362db2a7d8cae5b0b30b0";
+            public static final String reserve = "cxf58b9a1898998a31be7f1d99276204a3333ac9b3";
+            public static final String rewards = "cx10d59e8103ab44635190bd4139dbfd682fa2d07e";
+            public static final String router = "cx21e94c08c03daee80c25d8ee3ea22a20786ec231";
+            public static final String sicx = "cx2609b924e33ef00b648a409245c7ea394c467824";
+            public static final String staking = "cx43e2eec79eb76293c298f2b17aec06097be606e0";
+            public static final String multicall = "cxa4aa9185e23558cff990f494c1fd2845f6cbf741";
+            public static final String stakedlp =  "cx8dc674ce709c518bf1a6058a2722a59f94b6fb7f";
+      }
+}
