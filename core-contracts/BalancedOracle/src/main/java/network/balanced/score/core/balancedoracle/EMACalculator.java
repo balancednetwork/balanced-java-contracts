c3291afb938bb4661fd21c100ac2061ddef54b20
AntonAndell <anton@andell.eu>
2022-07-28 07:30:01 +0200

    copy balancedOracle from multi collateral branch

---
+++ core-contracts/BalancedOracle/src/main/java/network/balanced/score/core/balancedoracle/EMACalculator.java
@@ -0,0 +1,67 @@
+ 
+/*
+ * Copyright (c) 2022-2022 Balanced.network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package network.balanced.score.core.balancedoracle;
+import score.VarDB;
+import score.BranchDB;
+import score.Context;
+
+import static network.balanced.score.lib.utils.Constants.EXA;
+import static network.balanced.score.lib.utils.Math.exaPow;
+import java.math.BigInteger;
+
+public class EMACalculator {
+   private static final BranchDB<String, VarDB<BigInteger>> movingAverages = Context.newBranchDB("exponential_moving_averages", BigInteger.class);
+   private static final BranchDB<String, VarDB<BigInteger>> lastUpdateBlock = Context.newBranchDB("last_update_blocks", BigInteger.class);
+   private static final BranchDB<String, VarDB<BigInteger>> previousPrices = Context.newBranchDB("previous_prices", BigInteger.class);
+
+   public static BigInteger updateEMA(String symbol, BigInteger currentPrice, BigInteger alpha) {
+      VarDB<BigInteger> lastUpdate = lastUpdateBlock.at(symbol);
+      VarDB<BigInteger> movingAverage = movingAverages.at(symbol);
+      BigInteger lastBlock = lastUpdate.get();
+
+      if (lastBlock == null) {
+         BigInteger currentBlock = BigInteger.valueOf(Context.getBlockHeight());
+         lastUpdate.set(currentBlock);
+         movingAverage.set(currentPrice);
+         previousPrices.at(symbol).set(currentPrice);
+         
+         return currentPrice;
+      }
+
+      BigInteger currentBlock = BigInteger.valueOf(Context.getBlockHeight());
+      BigInteger blockDiff = currentBlock.subtract(lastBlock);
+      BigInteger currentMovingAverage = movingAverage.get();
+
+      if (blockDiff.equals(BigInteger.ZERO)) {
+         return currentMovingAverage;
+      }
+
+      VarDB<BigInteger> previousPrice = previousPrices.at(symbol);
+      BigInteger price = previousPrice.getOrDefault(BigInteger.ZERO);
+      previousPrice.set(currentPrice);
+
+      BigInteger weight = exaPow(EXA.subtract(alpha), blockDiff.intValue());
+      BigInteger priceChange = price.subtract(currentMovingAverage);
+      BigInteger newMovingAverge = price.subtract(priceChange.multiply(weight).divide(EXA));
+
+      lastUpdate.set(currentBlock);
+      movingAverage.set(newMovingAverge);
+
+      return newMovingAverge;
+   }
+}
---
+++ score-lib/src/main/java/network/balanced/score/lib/interfaces/BalancedOracle.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022-2022 Balanced.network.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package network.balanced.score.lib.interfaces;
+
+import network.balanced.score.lib.interfaces.addresses.*;
+import network.balanced.score.lib.interfaces.base.Name;
+import score.annotation.External;
+
+import java.math.BigInteger;
+
+import foundation.icon.score.client.ScoreClient;
+import foundation.icon.score.client.ScoreInterface;
+
+@ScoreClient
+@ScoreInterface
+public interface BalancedOracle extends 
+        Name,
+        GovernanceAddress,
+        AdminAddress,
+        DexAddress,
+        OracleAddress, 
+        StakingAddress {
+
+    @External
+    BigInteger getPriceInLoop(String symbol);
+
+    @External(readonly = true)
+    BigInteger getLastPriceInLoop(String symbol);
+
+    @External
+    void addDexPricedAsset(String symbol, BigInteger dexBnusdPoolId);
+
+    @External
+    void removeDexPricedAsset(String symbol);
+
+    @External(readonly = true)
+    BigInteger getAssetBnusdPoolId(String symbol);
+
+    @External
+    void setPeg(String symbol, String peg);
+
+    @External(readonly = true)
+    String getPeg(String symbol);
+
+    @External
+    void setDexPriceEMADecay(BigInteger decay);
+       
+    @External(readonly = true)
+    BigInteger getDexPriceEMADecay();
+    
+    @External
+    void setOraclePriceEMADecay(BigInteger decay);
+   
+    @External(readonly = true)
+    BigInteger getOraclePriceEMADecay();
+}
