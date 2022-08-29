f7a0b3d69026403533cb1699b4a2cecc740a30c8
AntonAndell <anton@andell.eu>
2022-08-29 13:26:38 +0200

    add test for total balance/debt verification

--- core-contracts/Loans/src/intTest/java/network/balanced/score/core/loans/LoansIntegrationTestBase.java
+++ core-contracts/Loans/src/intTest/java/network/balanced/score/core/loans/LoansIntegrationTestBase.java
@@ -18,8 +18,14 @@ package network.balanced.score.core.loans;
 
 import network.balanced.score.lib.test.integration.Balanced;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static network.balanced.score.lib.test.integration.BalancedUtils.hexObjectToBigInteger;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigInteger;
+import java.util.Map;
 
 class LoansIntegrationTestBase extends LoansIntegrationTest {
     @BeforeAll
@@ -30,5 +36,48 @@ class LoansIntegrationTestBase extends LoansIntegrationTest {
         reader = balanced.newClient(BigInteger.ZERO);
 
         LoansIntegrationTest.setup();
+    }
+
+    @Test
+    @Order(101)
+    void checkBalances() throws Exception {
+        BigInteger sICXBalance = BigInteger.ZERO;
+        BigInteger iETHBalance = BigInteger.ZERO;
+        BigInteger iBTCBalance = BigInteger.ZERO;
+        BigInteger bnUSDBalance = BigInteger.ZERO;
+        int i = 1;
+        while (true) {
+                score.Address address;
+                try {
+                        address = reader.loans.getPositionAddress(i);
+                } catch (Exception e) {
+                        break;
+                }
+                i++;
+
+                Map<String, Object> position = reader.loans.getAccountPositions(address);
+                Map<String, Map<String, Object>> assetsDetails = (Map<String, Map<String, Object>>) position.get("holdings");
+                if (assetsDetails.containsKey("sICX")) {
+                        sICXBalance = sICXBalance.add(hexObjectToBigInteger(assetsDetails.get("sICX").get("sICX")));
+                        bnUSDBalance = bnUSDBalance.add(hexObjectToBigInteger(assetsDetails.get("sICX").get("bnUSD")));
+                }
+
+                if (assetsDetails.containsKey("iETH")) {
+                        iETHBalance = iETHBalance.add(hexObjectToBigInteger(assetsDetails.get("iETH").get("iETH")));
+                        bnUSDBalance = bnUSDBalance.add(hexObjectToBigInteger(assetsDetails.get("iETH").get("bnUSD")));
+                }
+
+                if (assetsDetails.containsKey("iBTC")) {
+                        iBTCBalance = iBTCBalance.add(hexObjectToBigInteger(assetsDetails.get("iBTC").get("iBTC")));
+                        bnUSDBalance = bnUSDBalance.add(hexObjectToBigInteger(assetsDetails.get("iBTC").get("bnUSD")));
+                }
+        }
+
+        assertEquals(sICXBalance, reader.sicx.balanceOf(balanced.loans._address()));
+        assertEquals(iETHBalance, reader.irc2(ethAddress).balanceOf(balanced.loans._address()));
+        assertEquals(iBTCBalance, reader.irc2(btcAddress).balanceOf(balanced.loans._address()));
+
+        BigInteger stabilityDebt = reader.sicx.balanceOf(balanced.stability._address());
+        assertEquals(bnUSDBalance, reader.bnUSD.totalSupply().subtract(stabilityDebt));
     }
 }
