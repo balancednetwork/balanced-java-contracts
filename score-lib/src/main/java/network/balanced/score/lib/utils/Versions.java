5dcd711a90792bbd98903233a0296716b07ee03d
AntonAndell <anton@andell.eu>
2023-03-23 10:58:51 +0100

    Implement bnUSD only version of OTC contract (#307)
    * Implement bnUSD only version of OTC contract
    
    * Move OTC contract to util contracts
    
    * style: auto format code
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    
    * refactor: rename package name to util folder
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    
    * refactor: optimize code
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    
    * docs: fix grammatical issue in readme
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    
    ---------
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    Co-authored-by: Night Owl <nightowl121@protonmail.com>

--- score-lib/src/main/java/network/balanced/score/lib/utils/Versions.java
+++ score-lib/src/main/java/network/balanced/score/lib/utils/Versions.java
@@ -37,4 +37,5 @@ public class Versions {
     public final static String STAKEDLP = "v1.0.0";
     public final static String BOOSTED_BALN = "v1.0.0";
     public final static String BRIBING = "v1.0.0";
+    public final static String BALANCED_OTC = "v1.0.0";
 }
