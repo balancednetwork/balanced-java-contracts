9019982c88838ac5f70a6756f0d42fa790179b40
naneey <128459422+naneey@users.noreply.github.com>
2023-03-27 21:15:24 +0545

    Audit changes for staking sicx (#308)
    * audit changes for mintTo
    
    * staking contract version update

--- score-lib/src/main/java/network/balanced/score/lib/utils/Versions.java
+++ score-lib/src/main/java/network/balanced/score/lib/utils/Versions.java
@@ -22,7 +22,7 @@ public class Versions {
     public final static String LOANS = "v1.0.0";
     public final static String RESERVE = "v1.0.0";
     public final static String SICX = "v1.0.0";
-    public final static String STAKING = "v1.0.0";
+    public final static String STAKING = "v1.0.1";
     public final static String WORKERTOKEN = "v1.0.0";
     public final static String BNUSD = "v1.0.0";
     public final static String FEEHANDLER = "v1.0.0";
