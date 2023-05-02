ee6b14eeeafe4cdc9c29fd50b7c48960c2aae503
Night Owl <91905208+nightowl121@users.noreply.github.com>
2023-05-02 14:41:26 +0545

    feat: implement new bbaln penalty structure (#312)
    * feat: implement new bbaln penalty structure
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    
    * chore: upgrade version of bbaln token
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    
    * Add new unit test to check both cases, move penalty logic to before position is closed
    
    ---------
    
    Signed-off-by: Night Owl <nightowl121@protonmail.com>
    Co-authored-by: AntonAndell <anton@andell.eu>

--- score-lib/src/main/java/network/balanced/score/lib/utils/Versions.java
+++ score-lib/src/main/java/network/balanced/score/lib/utils/Versions.java
@@ -35,7 +35,7 @@ public class Versions {
     public final static String REBALANCING = "v1.0.0";
     public final static String ROUTER = "v1.0.0";
     public final static String STAKEDLP = "v1.0.0";
-    public final static String BOOSTED_BALN = "v1.0.0";
+    public final static String BOOSTED_BALN = "v1.1.0";
     public final static String BRIBING = "v1.0.0";
     public final static String BALANCED_OTC = "v1.0.0";
 }
